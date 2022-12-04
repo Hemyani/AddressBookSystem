2fea60c0f8a0aca39510794103a91707cb307832
Hemyani <hdg2512@gmail.com>
2022-12-04 16:54:35 +0530

    Ability to create a Contacts in Address Book

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -11,166 +11,28 @@ namespace AddressBookSystem
     {
         public static void Main(string[] args)
         {
-            //UC1
-            //Console.WriteLine("Welcome To Address Book ");
-            //Create person = new Create();
-            //while (true)
-            //{
-            //    Console.WriteLine("Please enter a command: ");
-            //    string command = Console.ReadLine();
-            //    Console.WriteLine("Entered Command is " + command);
+            Console.WriteLine("Welcome To Address Book ");
+            Create person = new Create();
+            while (true)
+            {
+                Console.WriteLine("Please enter a command: ");
+                string command = Console.ReadLine();
+                Console.WriteLine("Entered Command is " + command);
 
-            //    if (command == "add")
-            //    {
-            //        Create.AddPerson();
-            //    }
-            //    if (command == "view")
-            //    {
-            //        Create.listPerson();
-            //    }
+                if (command == "add")
+                {
+                    Create.AddPerson();
+                }
+                if (command == "view")
+                {
+                    Create.listPerson();
+                }
 
-            //    if (command == "ViewDictionary")
-            //    {
-            //        person.Display();
-            //    }
-            //}
-
-
-            //UC2
-            //Console.WriteLine("Welcome To Address Book ");
-            //NewContact user = new NewContact();
-            //while (true)
-            //{
-            //    Console.WriteLine("Please enter a command: ");
-            //    string command = Console.ReadLine();
-            //    Console.WriteLine("Entered Command is " + command);
-
-            //    if (command == "add")
-            //    {
-            //        NewContact.AddPerson();
-            //    }
-            //    if (command == "view")
-            //    {
-            //        NewContact.listPerson();
-            //    }
-            //    if (command == "UniqueBookName")
-            //    {
-            //        user.NewUser();
-            //    }
-
-            //    if (command == "ViewDictionary")
-            //    {
-            //        user.Display();
-            //    }
-            //}
-
-            //UC3
-            //Console.WriteLine("Welcome To Address Book");
-            //EditContact edit = new EditContact();
-            //while (true)
-            //{
-            //    Console.WriteLine("Please enter a command: ");
-            //    string command = Console.ReadLine();
-            //    Console.WriteLine("Entered Command is " + command);
-
-            //    if (command == "add")
-            //    {
-            //        EditContact.AddPerson();
-            //    }
-            //    if (command == "view")
-            //    {
-            //        EditContact.listPerson();
-            //    }
-            //    if (command == "edit")
-            //    {
-            //        EditContact.EditPerson();
-            //    }
-            //    if (command == "UniqueBookName")
-            //    {
-            //        edit.NewUser();
-            //    }
-
-            //    if (command == "ViewDictionary")
-            //    {
-            //        edit.Display();
-            //    }
-            //}
-
-            //UC4
-            //Console.WriteLine("Welcome To Address Book ");
-            //DeletePerson obj = new DeletePerson();
-            //while (true)
-            //{
-            //    Console.WriteLine("Please enter a command: ");
-            //    string command = Console.ReadLine();
-            //    Console.WriteLine("Entered Command is " + command);
-
-            //    if (command == "add")
-            //    {
-            //        DeletePerson.AddPerson();
-            //    }
-            //    if (command == "view")
-            //    {
-            //        DeletePerson.listPerson();
-            //    }
-            //    if (command == "edit")
-            //    {
-            //        DeletePerson.EditPerson();
-            //    }
-            //    if (command == "delete")
-            //    {
-            //        DeletePerson.DeleteContact();
-            //    }
-            //    if (command == "UniqueBookName")
-            //    {
-            //        obj.NewUser();
-            //    }
-
-            //    if (command == "ViewDictionary")
-            //    {
-            //        obj.Display();
-            //    }
-            //}
-
-            //UC5
-            //Console.WriteLine("Welcome To Address Book ");
-            //MultiplePerson multiple = new MultiplePerson();
-            //while (true)
-            //{
-            //    Console.WriteLine("Please enter a command: ");
-            //    string command = Console.ReadLine();
-            //    Console.WriteLine("Entered Command is " + command);
-
-            //    if (command == "add")
-            //    {
-            //        MultiplePerson.AddPerson();
-            //    }
-            //    if (command == "view")
-            //    {
-            //        MultiplePerson.listPerson();
-            //    }
-            //    if (command == "edit")
-            //    {
-            //        MultiplePerson.EditPerson();
-            //    }
-            //    if (command == "delete")
-            //    {
-            //        MultiplePerson.DeleteContact();
-            //    }
-            //    if (command == "AddMultiplePerson")
-            //    {
-            //        MultiplePerson.MultipleContacts();
-            //    }
-            //    if (command == "UniqueBookName")
-            //    {
-            //        multiple.NewUser();
-            //    }
-
-            //    if (command == "ViewDictionary")
-            //    {
-            //        multiple.Display();
-            //    }
-            //}
+                if (command == "ViewDictionary")
+                {
+                    person.Display();
+                }
+            }
         }
     }
 }
